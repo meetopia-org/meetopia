02e1b92dc7e93508edaa99945184aa7ed80182ae
Camilo Gonzalez <Camilo.Gonzalez@blackbirdlabs.com.co>
2025-08-30 17:37:42 -0500

    feat: Creates base Activity entity

---
+++ tests/Meetopia.Domain.Tests/Activities/ActivityTests.cs
@@ -0,0 +1,23 @@
+using Meetopia.Domain.Activities.Entities;
+
+namespace Meetopia.Domain.Tests.Activities;
+
+public class ActivityTests
+{
+    [Fact]
+    public void Activity_Creation_Sets_Id()
+    {
+        var activity = new Activity
+        {
+            Title = "Sample Title",
+            Description = "Sample Description",
+            Category = "Sample Category",
+            City = "Sample City",
+            Venue = "Sample Venue"
+        };
+
+        var id = activity.Id;
+
+        id.Should().NotBeNullOrEmpty();
+    }
+}
